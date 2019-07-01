62c1ece69f83c2ff37e61286c4c2df804309e4a8
open-analysis <jchica79@comcast.net>
2019-07-01 10:55:13 -0500

    added info.php and add_warehouse.php to git

---
+++ info.php
@@ -0,0 +1,64 @@
+<html>
+<body>
+	<title>Test</title>
+
+	<?php
+#hello
+		$link = mysqli_connect("localhost", "root", "3ng1neering");
+
+		if(!$link){
+			die("ERROR: Could not connect. " . mysqli_connect_error());
+			echo "<br>";
+		}
+
+		$sql = "CREATE DATABASE IF NOT EXISTS warehouse";
+	        if (mysqli_query($link, $sql)){
+	                echo "Database warehouse created";
+	        } else {
+	                echo "Database failed to be created\n" . mysqli_error($link);
+	        }
+
+		echo "<br>";
+	/*
+		$sql = "DROP DATABASE IF EXISTS demo";
+		if (mysqli_query($link, $sql)){
+			echo "Database deleted";
+		} else {
+			echo "Database failed to delete\n" . mysqli_error($link);
+		}
+	*/
+	//	mysqli_close($link);
+
+		?>
+
+		<h2>Add items to warehouse:</h2>
+		<form method="POST" action="add_warehouse.php">
+		Warehouse ID: <input type="text" name="id">
+		Warehouse Amount: <input type="text" name="amount">
+		<input type="submit" value="Submit">
+		</form>
+
+		<br>
+
+		<h2>Add items to floor:</h2>
+	        <form method="POST" action="add_floor.php">
+	        Warehouse ID: <input type="text" name="id">
+	        Warehouse Amount: <input type="text" name="amount">
+	        <input type="submit" value="Submit">
+	        </form>
+
+		<br>
+
+	        <h2>Add items to line:</h2>
+	        <form method="POST" action="add_line.php">
+	        Warehouse ID: <input type="text" name="id">
+	        Warehouse Amount: <input type="text" name="amount">
+	        <input type="submit" value="Submit">
+	        </form>
+
+
+		<?php 
+			mysqli_close($link);
+		?>
+</body>
+</html>
