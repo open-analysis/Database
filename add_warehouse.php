9c70226d2aafcee9b61b472b7daef0739924f161
open-analysis <jchica79@comcast.net>
2019-07-01 11:22:38 -0500

    fixed add_warehouse to be the proper script

--- add_warehouse.php
+++ add_warehouse.php
@@ -1,64 +1,40 @@
-<html>
-<body>
-	<title>Test</title>
+<?php
+	$link = mysqli_connect("localhost", "root", "3ng1neering");
 
-	<?php
-#hello
-		$link = mysqli_connect("localhost", "root", "3ng1neering");
+        if(!$link){
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
 
-		if(!$link){
-			die("ERROR: Could not connect. " . mysqli_connect_error());
-			echo "<br>";
-		}
-
-		$sql = "CREATE DATABASE IF NOT EXISTS warehouse";
-	        if (mysqli_query($link, $sql)){
-	                echo "Database warehouse created";
-	        } else {
-	                echo "Database failed to be created\n" . mysqli_error($link);
-	        }
-
-		echo "<br>";
-	/*
-		$sql = "DROP DATABASE IF EXISTS demo";
+	if (mysqli_select_db($link, "warehouse")){
+		echo "selected warehouse database";
+		$sql = "CREATE TABLE IF NOT EXISTS storage (id INTEGER, amount INTEGER)";
 		if (mysqli_query($link, $sql)){
-			echo "Database deleted";
+			echo "<br>created table storage";
+			// add item
+			$sql = "INSERT INTO storage (id, amount) VALUES (" . $_POST['id'] . ", " . $_POST['amount'] . ")";
+            if (mysqli_query($link, $sql)){
+	                echo "<br>Added: " . $_POST['amount'] . " of " . $_POST['id'] . " to storage";
+        	} else {
+                	echo "<br>didn't add values to storage " . mysqli_error($link);
+            }
+            // update item
+            $sql = "UPDATE storage SET amount=600 WHERE id=" . $_POST['id'];
+            if (mysqli_query($link, $sql)){
+	                echo "<br>Updated: " . $_POST['id'] . " to have 6 items in storage";
+        	} else {
+                	echo "<br>didn't update values to storage " . mysqli_error($link);
+            }
+            // delete the whole item probably unneeded unless a whole product is being discontinued.
+            $sql = "DELETE FROM storage WHERE " . $_POST['id'];
+            if (mysqli_query($link, $sql)){
+	                echo "<br>Deleted: " . $_POST['id'] . " from storage";
+        	} else {
+                	echo "<br>didn't delete values to storage " . mysqli_error($link);
+            }
 		} else {
-			echo "Database failed to delete\n" . mysqli_error($link);
+			echo "<br>didn't create table " . mysqli_error($link);
 		}
-	*/
-	//	mysqli_close($link);
-
-		?>
-
-		<h2>Add items to warehouse:</h2>
-		<form method="POST" action="add_warehouse.php">
-		Warehouse ID: <input type="text" name="id">
-		Warehouse Amount: <input type="text" name="amount">
-		<input type="submit" value="Submit">
-		</form>
-
-		<br>
-
-		<h2>Add items to floor:</h2>
-	        <form method="POST" action="add_floor.php">
-	        Warehouse ID: <input type="text" name="id">
-	        Warehouse Amount: <input type="text" name="amount">
-	        <input type="submit" value="Submit">
-	        </form>
-
-		<br>
-
-	        <h2>Add items to line:</h2>
-	        <form method="POST" action="add_line.php">
-	        Warehouse ID: <input type="text" name="id">
-	        Warehouse Amount: <input type="text" name="amount">
-	        <input type="submit" value="Submit">
-	        </form>
-
-
-		<?php 
-			mysqli_close($link);
-		?>
-</body>
-</html>
+	} else {
+		echo "failed to select database " . mysqli_error($link);
+	}
+?>
